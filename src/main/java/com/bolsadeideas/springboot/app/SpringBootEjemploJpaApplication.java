99e8545df35b8c4c7a6f69dd30b272c7ef7bd75d
Plasencia <plasenciavillarejo@gmail.com>
2021-09-20 20:08:55 +0200

    Se aniade CommandLineRunner para ver la password encriptada en Bcrypt en la clase SpringBootEjemploJpaApplication.java

--- src/main/java/com/bolsadeideas/springboot/app/SpringBootEjemploJpaApplication.java
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootEjemploJpaApplication.java
@@ -1,13 +1,32 @@
 package com.bolsadeideas.springboot.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class SpringBootEjemploJpaApplication {
+public class SpringBootEjemploJpaApplication  implements CommandLineRunner{
 
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootEjemploJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		String password = "12345";
+		
+		for(int i=0; i<2; i ++) {
+			String encriptacion = passwordEncoder.encode(password);
+			System.out.println("La encriptción para la pas -> 12345 en Bcryps es:" + encriptacion);
+		}
+		
+		
 	}
 
 }
